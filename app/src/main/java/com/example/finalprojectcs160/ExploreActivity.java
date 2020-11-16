96f1e0947162fb35a3772081fb76700cd5f803a2
Vincent Tantra <v_tantra@berkeley.edu>
2020-11-16 03:48:43 -0800

    Add start of bottom navbar structure, commented out FAB

--- app/src/main/java/com/example/finalprojectcs160/ExploreActivity.java
+++ app/src/main/java/com/example/finalprojectcs160/ExploreActivity.java
@@ -19,13 +19,13 @@ public class ExploreActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 }
