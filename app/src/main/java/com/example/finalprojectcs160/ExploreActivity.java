dc889f511e6057fb8b5a5ca0a939fcd8cbbc7405
Vincent Tantra <v_tantra@berkeley.edu>
2020-11-16 03:51:55 -0800

    Remove top toolbar

--- app/src/main/java/com/example/finalprojectcs160/ExploreActivity.java
+++ app/src/main/java/com/example/finalprojectcs160/ExploreActivity.java
@@ -16,8 +16,8 @@ public class ExploreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explore);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
 //        FloatingActionButton fab = findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
