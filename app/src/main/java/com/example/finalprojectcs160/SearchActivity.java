b29e57aa93ec3e4e1316baaab3184d025e95f784
Vincent Tantra <v_tantra@berkeley.edu>
2020-11-18 11:21:33 -0800

    Add ellipse drawable

--- app/src/main/java/com/example/finalprojectcs160/SearchActivity.java
+++ app/src/main/java/com/example/finalprojectcs160/SearchActivity.java
@@ -21,29 +21,37 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         ImageView category_im_1 = findViewById(R.id.category_im_1);
-        Glide.with(this).load("https://i.imgur.com/ZcLLrkY.jpg")
-                .apply(new RequestOptions().override(300, 300)).into(category_im_1);
+        category_im_1.setImageResource(R.drawable.groceries);
+//        Glide.with(this).load("https://i.imgur.com/ZcLLrkY.jpg")
+//                .apply(new RequestOptions().override(300, 300)).into(category_im_1);
         ImageView category_im_2 = findViewById(R.id.category_im_2);
-        Glide.with(this).load("https://i.imgur.com/ZcLLrkY.jpg")
-                .apply(new RequestOptions().override(300, 300)).into(category_im_2);
+        category_im_2.setImageResource(R.drawable.clothing);
+//        Glide.with(this).load("https://i.imgur.com/ZcLLrkY.jpg")
+//                .apply(new RequestOptions().override(300, 300)).into(category_im_2);
         ImageView category_im_3 = findViewById(R.id.category_im_3);
-        Glide.with(this).load("https://i.imgur.com/ZcLLrkY.jpg")
-                .apply(new RequestOptions().override(300, 300)).into(category_im_3);
+        category_im_3.setImageResource(R.drawable.bathbody);
+//        Glide.with(this).load("https://i.imgur.com/ZcLLrkY.jpg")
+//                .apply(new RequestOptions().override(300, 300)).into(category_im_3);
         ImageView category_im_4 = findViewById(R.id.category_im_4);
-        Glide.with(this).load("https://i.imgur.com/ZcLLrkY.jpg")
-                .apply(new RequestOptions().override(300, 300)).into(category_im_4);
+        category_im_4.setImageResource(R.drawable.restaurants);
+//        Glide.with(this).load("https://i.imgur.com/ZcLLrkY.jpg")
+//                .apply(new RequestOptions().override(300, 300)).into(category_im_4);
         ImageView category_im_5 = findViewById(R.id.category_im_5);
-        Glide.with(this).load("https://i.imgur.com/ZcLLrkY.jpg")
-                .apply(new RequestOptions().override(300, 300)).into(category_im_5);
+        category_im_5.setImageResource(R.drawable.services);
+//        Glide.with(this).load("https://i.imgur.com/ZcLLrkY.jpg")
+//                .apply(new RequestOptions().override(300, 300)).into(category_im_5);
         ImageView category_im_6 = findViewById(R.id.category_im_6);
-        Glide.with(this).load("https://i.imgur.com/ZcLLrkY.jpg")
-                .apply(new RequestOptions().override(300, 300)).into(category_im_6);
-        ImageView category_im_7 = findViewById(R.id.category_im_7);
-        Glide.with(this).load("https://i.imgur.com/ZcLLrkY.jpg")
-                .apply(new RequestOptions().override(300, 300)).into(category_im_7);
-        ImageView category_im_8 = findViewById(R.id.category_im_8);
-        Glide.with(this).load("https://i.imgur.com/ZcLLrkY.jpg")
-                .apply(new RequestOptions().override(300, 300)).into(category_im_8);
+        category_im_6.setImageResource(R.drawable.other);
+//        Glide.with(this).load("https://i.imgur.com/ZcLLrkY.jpg")
+//                .apply(new RequestOptions().override(300, 300)).into(category_im_6);
+//        ImageView category_im_7 = findViewById(R.id.category_im_7);
+//        category_im_1.setImageResource(R.drawable.groceries);
+//        Glide.with(this).load("https://i.imgur.com/ZcLLrkY.jpg")
+//                .apply(new RequestOptions().override(300, 300)).into(category_im_7);
+//        ImageView category_im_8 = findViewById(R.id.category_im_8);
+//        category_im_1.setImageResource(R.drawable.groceries);
+//        Glide.with(this).load("https://i.imgur.com/ZcLLrkY.jpg")
+//                .apply(new RequestOptions().override(300, 300)).into(category_im_8);
 
 //        Toolbar toolbar = findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
